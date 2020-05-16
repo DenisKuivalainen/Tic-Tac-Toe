c566a2d6c79921404a89b464f6388c9e3c10ece0
Kuivalainen <54420379+DenisKuivalainen@users.noreply.github.com>
2020-05-16 16:50:24 +0300

    X! O! Super! v1.2

--- Field.java
+++ Field.java
@@ -10,7 +10,7 @@ public class Field extends JFrame implements ActionListener {
     JLabel label;
     // As I add, remove and rewrite elements, I supposed to use Arrays instead ArrayList or LinkedList, despite their adventures
     // As swing does not support ID for elements, I use array for this purpose
-    JButton[] btn = new JButton[25];
+    JButton[][] btn = new JButton[5][5];
     JButton resetBtn;
     JButton modeBtn;
     JPanel panel;
@@ -45,90 +45,20 @@ public class Field extends JFrame implements ActionListener {
         modeBtn.setBounds(290, 10, 60, 30);
         modeBtn.addActionListener(this);
 
-        btn[0] = new JButton("");
-        btn[0].setBounds(10, 60, 60, 60);
-
-        btn[1] = new JButton("");
-        btn[1].setBounds(80, 60, 60, 60);
-
-        btn[2] = new JButton("");
-        btn[2].setBounds(150, 60, 60, 60);
-
-        btn[3] = new JButton("");
-        btn[3].setBounds(220, 60, 60, 60);
-
-        btn[4] = new JButton("");
-        btn[4].setBounds(290, 60, 60, 60);
-
-        btn[5] = new JButton("");
-        btn[5].setBounds(10, 130, 60, 60);
-
-        btn[6] = new JButton("");
-        btn[6].setBounds(80, 130, 60, 60);
-
-        btn[7] = new JButton("");
-        btn[7].setBounds(150, 130, 60, 60);
-
-        btn[8] = new JButton("");
-        btn[8].setBounds(220, 130, 60, 60);
-
-        btn[9] = new JButton("");
-        btn[9].setBounds(290, 130, 60, 60);
-
-        btn[10] = new JButton("");
-        btn[10].setBounds(10, 200, 60, 60);
-
-        btn[11] = new JButton("");
-        btn[11].setBounds(80, 200, 60, 60);
-
-        btn[12] = new JButton("");
-        btn[12].setBounds(150, 200, 60, 60);
-
-        btn[13] = new JButton("");
-        btn[13].setBounds(220, 200, 60, 60);
-
-        btn[14] = new JButton("");
-        btn[14].setBounds(290, 200, 60, 60);
-
-        btn[15] = new JButton("");
-        btn[15].setBounds(10, 270, 60, 60);
-
-        btn[16] = new JButton("");
-        btn[16].setBounds(80, 270, 60, 60);
-
-        btn[17] = new JButton("");
-        btn[17].setBounds(150, 270, 60, 60);
-
-        btn[18] = new JButton("");
-        btn[18].setBounds(220, 270, 60, 60);
-
-        btn[19] = new JButton("");
-        btn[19].setBounds(290, 270, 60, 60);
-
-        btn[20] = new JButton("");
-        btn[20].setBounds(10, 340, 60, 60);
-
-        btn[21] = new JButton("");
-        btn[21].setBounds(80, 340, 60, 60);
-
-        btn[22] = new JButton("");
-        btn[22].setBounds(150, 340, 60, 60);
-
-        btn[23] = new JButton("");
-        btn[23].setBounds(220, 340, 60, 60);
-
-        btn[24] = new JButton("");
-        btn[24].setBounds(290, 340, 60, 60);
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                btn[i][j] = new JButton("");
+                btn[i][j].setBounds(10 + j * 70, 60 + i * 70, 60, 60);
+                btn[i][j].addActionListener(this);
+                panel.add(btn[i][j]);
+            }
+        }
 
         labelTurn();
 
         panel.add(label);
         panel.add(resetBtn);
         panel.add(modeBtn);
-        for(int i = 0; i < 25; i++){
-            btn[i].addActionListener(this);
-            panel.add(btn[i]);
-        }
         this.add(panel);
     }
 
@@ -142,9 +72,11 @@ public class Field extends JFrame implements ActionListener {
         } else if(e.getActionCommand().equals("PP") || e.getActionCommand().equals("AI")) {
             clickMode();
         } else if(playable) {
-            for (int i = 0; i < 25; i++) {
-                if (e.getSource() == btn[i]) {
-                    pos = i;
+            for(int i = 0; i < 5; i++) {
+                for(int j = 0; j < 5; j++) {
+                    if (e.getSource() == btn[i][j]) {
+                        pos = i * 5 + j;
+                    }
                 }
             }
             if(xo[pos] != "X" && xo[pos] != "O") {
@@ -164,8 +96,10 @@ public class Field extends JFrame implements ActionListener {
     // Заменяет значение кнопки а на str
     // Change button text value
     void changerOne(int a, String str){
+        int i = (int) (Math.floor(a / 5));
+        int j = a % 5;
         xo[a] = str;
-        btn[a].setText(str);
+        btn[i][j].setText(str);
     }
 
     // Убирает значения всех кнопок
